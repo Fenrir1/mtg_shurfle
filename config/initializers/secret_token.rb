d1148341fffc753a55cd3ddb2779e34375277991
Fenrir <fenrir.new@gmail.com>
2012-04-20 21:25:56 +0600

    Delete All

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-MtgShurfle::Application.config.secret_token = '75fd2f275d79271572864c97b8904fce9b4e77e80208242c048c66dafa178a2693bf7c136fca6de4295286a83943fb01f79a85e50ebdab6ff7e97379933d2d56'
