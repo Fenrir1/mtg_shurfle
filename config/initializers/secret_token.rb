9c302f9586fe8c3e4d9605671ef40985e7e0e9e2
Fenrir <fenrir.new@gmail.com>
2012-04-22 18:12:57 +0600

    Next try

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TempApp::Application.config.secret_token = '1ccf9df3b787fa425acd16fb82712ddecfca5a5dcd240ae961bd7fb9cf0075b84f85b0593c92782a2d7590fa54c2684170533ab56811d07a814a3fc7cde3b309'
